96af8e9e5cd2771ea2f08a5e7494c3f51ddbd2d4
thecodingmontana <dev.humblechris254@gmail.com>
2025-04-10 12:52:02 +0300

    chore(app): add tailwindcssv4 wih shadcn-vue and custom font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+import tailwindcss from '@tailwindcss/vite'
+
 export default defineNuxtConfig({
 
   modules: [
@@ -6,12 +8,20 @@ export default defineNuxtConfig({
     '@nuxt/fonts',
     '@nuxt/icon',
     '@nuxt/image',
+    'shadcn-nuxt',
   ],
   devtools: { enabled: true },
+  css: ['~/assets/css/tailwind.css'],
   future: {
     compatibilityVersion: 4,
   },
   compatibilityDate: '2024-11-01',
+  vite: {
+    plugins: [
+      // @ts-expect-error just ignore its not an error
+      tailwindcss(),
+    ],
+  },
   eslint: {
     config: {
       stylistic: true,
@@ -21,5 +31,16 @@ export default defineNuxtConfig({
     families: [
       { name: 'Geist', provider: 'google' },
     ],
+  },
+  shadcn: {
+    /**
+     * Prefix for all the imported component
+     */
+    prefix: '',
+    /**
+     * Directory that the component lives in.
+     * @default "./components/ui"
+     */
+    componentDir: './components/ui',
   },
 })
