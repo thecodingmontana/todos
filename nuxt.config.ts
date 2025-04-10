a5bd0d92e503edfd220c0c22f4491c008091b777
thecodingmontana <dev.humblechris254@gmail.com>
2025-04-10 12:13:55 +0300

    chore(app): add eslint

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,12 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-11-01',
-  devtools: { enabled: true },
 
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
     '@nuxt/icon',
-    '@nuxt/image'
-  ]
+    '@nuxt/image',
+  ],
+  devtools: { enabled: true },
+  future: {
+    compatibilityVersion: 4,
+  },
+  compatibilityDate: '2024-11-01',
+  eslint: {
+    config: {
+      stylistic: true,
+    },
+  },
+  fonts: {
+    families: [
+      { name: 'Geist', provider: 'google' },
+    ],
+  },
 })
